8dd1a6282a1bfd97948cf69154d4479695628869
fernandaguzzo <guzzofernanda84@gmail.com>
2025-05-04 22:21:05 -0300

    chamada HTTP Get

--- crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-
+  list(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      tap(courses => console.log(courses)) 
+    );
+  }
 }
+
