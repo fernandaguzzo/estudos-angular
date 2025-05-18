b2afb9a3dcbf54d5cb6ba278c340283bf466d590
fernandaguzzo <guzzofernanda84@gmail.com>
2025-05-17 22:13:36 -0300

    tratamento de erros e MatDialog

--- crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -10,13 +12,27 @@ import { Observable } from 'rxjs';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService){
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ){
 
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list().pipe(
+      catchError(error => {
+       this.onError('Erro ao carregar cursos.')
+        return of([]) //criação de um observable que vai retornar um array vazio
+      })
+    );
+  }
+
+  onError(errorMsg: string){
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    })
   }
 
   ngOnInit(): void {
--- crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular-spring/crud-angular/src/app/courses/services/courses.service.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, first, delay } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/assets/acourses.json';
 
   constructor(private httpClient: HttpClient) { }
 
   list(): Observable<Course[]> {
-    return this.httpClient.get<Course[]>(this.API).pipe(
-      tap(courses => console.log(courses)) 
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(60000),
+      tap(courses => console.log(courses))
     );
   }
 }
